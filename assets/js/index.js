c4060361dcfd25fc1cc4a30befcead3d479bd48f
masuddev2 <tuhin.semicolonit@gmail.com>
2023-10-10 12:32:45 +0600

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,27 @@
-console.log("Masud Pervez");
+const product = {
+  type: "pen",
+  price: 10,
+  color: "black",
+  name: "i teen",
+  qty: 50,
+  forSale: true
+}
+
+const item = document.getElementById('product')
+
+function showProduct(name) {
+  return item.innerHTML = `
+    <div class="card">
+                    <div class="card-header">
+                        ${name}
+                    </div>
+                    <div class="card-body">
+                        <p>${product.type}</p>
+                        <p>${product.color}</p>
+                    </div>
+                </div>
+    `
+}
+
+
+
