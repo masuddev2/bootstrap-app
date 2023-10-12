eaffd4b9c9e91cef90defb16ad0bae28d260bdc7
masuddev2 <tuhin.semicolonit@gmail.com>
2023-10-12 12:47:44 +0600

    aa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,48 @@
-const product = {
-  type: "pen",
-  price: 10,
-  color: "black",
-  name: "i teen",
-  qty: 50,
-  forSale: true
-}
+// const person = {
+//   firstName: "Masud",
+//   lastName: "Pervez",
+//   fullName: function(){
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
 
-const item = document.getElementById('product')
+// console.log(person.fullName());
 
-function showProduct(name) {
-  return item.innerHTML = `
-    <div class="card">
-                    <div class="card-header">
-                        ${name}
-                    </div>
-                    <div class="card-body">
-                        <p>${product.type}</p>
-                        <p>${product.color}</p>
-                    </div>
-                </div>
-    `
-}
+// const myArr = ['a', 'c', 'b', 'd', 'e', 'c']
+// const myArr2 = [...myArr]
+
+// console.log("before",myArr);
+
+// myArr[2] = "book"
+
+// console.log("after",myArr);
 
 
+// ! not gate
+// || or gate
+// && and gate
 
+// console.log((true && true));
+// let ludo = (Math.random() * 7).toFixed(0)
+
+// console.log(ludo);
+
+
+// let text = "Hello"; 
+// text = text + " World";
+// console.log(text);
+// const newData =  myName.length
+// console.log(newData);
+
+
+// let age = 0
+
+// if (age >= 18) {
+//   console.log("Young");
+// } else if (age >= 1) {
+//   console.log("child");
+// } else {
+//   console.log("Not Born");
+// }
+
+// 100 mark 
